b3f90f8f4a89d5032bfca390f7ece23f5b09208b
shubhamdhamane <A00257743@mycambrian.ca>
2022-12-15 02:40:23 -0500

    For movie list controller and view model added

---
+++ Cinematic/bin/Debug/net7.0/fab259cb379d41d4ae8058f70a7ffa80.sh
@@ -0,0 +1,30 @@
+function list_child_processes () {
+    local ppid=$1;
+    local current_children=$(pgrep -P $ppid);
+    local local_child;
+    if [ $? -eq 0 ];
+    then
+        for current_child in $current_children
+        do
+          local_child=$current_child;
+          list_child_processes $local_child;
+          echo $local_child;
+        done;
+    else
+      return 0;
+    fi;
+}
+
+ps 83598;
+while [ $? -eq 0 ];
+do
+  sleep 1;
+  ps 83598 > /dev/null;
+done;
+
+for child in $(list_child_processes 83606);
+do
+  echo killing $child;
+  kill -s KILL $child;
+done;
+rm /Users/shubhamdhamane/Documents/cambrian/sem2/adv web/Final_project/Cinematic/Cinematic/bin/Debug/net7.0/fab259cb379d41d4ae8058f70a7ffa80.sh;

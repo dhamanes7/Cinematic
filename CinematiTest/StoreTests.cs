cc8cc811a85992a168ec94e01c11707914cd4520
gurpreetkaur <A00252915@mycambrian.ca>
2022-12-16 23:57:38 -0500

    TestADD added

--- CinematiTest/StoreTests.cs
+++ CinematiTest/StoreTests.cs
@@ -1,11 +1,110 @@
-﻿using System;
-namespace CinematicTest
+﻿using Cinematic.Models;
+using CinematicLibrary.DataSource.Store;
+using CinematicLibrary.Models;
+using Moq;
+
+namespace LibraryTest
 {
-	public class StoreTests
-	{
-		public StoreTests()
-		{
-		}
-	}
+    internal class MockStoreWrapper
+    {
+        public static Movie movieToAdd { get; } = new Movie
+        {
+            emsId = "4",
+            name = "Creed III",
+            releaseDate = "March 3, 2023",
+            posterImage = new PosterImage { Id = 4, url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGJmpojhOWQFz93yVXm24txNjXKQMIu60CZMep0NMb&usqp=CAE&s" },
+            User = new AppUser
+            {
+                UserName = "User3"
+            }
+        };
+
+        public static AppUser user2 = new AppUser
+        {
+            UserName = "User2"
+        };
+
+        public static Movie movieToDelete = new Movie
+        {
+            emsId = "3",
+            name = "Oppenheimer",
+            releaseDate = "July 21, 2023",
+            posterImage = new PosterImage { Id = 3, url = "https://movies.universalpictures.com/media/oppenheimer-poster-560x880-62defacb1b002-1.jpg" },
+            User = user2
+        };
+
+        public static string username = "User1";
+
+        public static Mock<IStore<Movie>> GetMock()
+        {
+            var mock = new Mock<IStore<Movie>>();
+
+            var user1 = new AppUser
+            {
+                UserName = username
+            };
+
+
+
+            // Setup the mock
+            var movies = new List<Movie>()
+            {
+                new Movie
+                {
+                    emsId = "1",
+                    name = "The Equalizer 3",
+                    releaseDate = "September 1, 2023",
+                    posterImage = new PosterImage{Id = 1, url = "http://www.impawards.com/tv/posters/equalizer_ver3.jpg"},
+                    User = user1
+                },
+                new Movie
+                {
+                    emsId = "2",
+                    name = "John Wick: Chapter 4",
+                    releaseDate = "March 24, 2023",
+                    posterImage = new PosterImage{Id = 2, url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-Z_6poRGqbhXsv62QgLmsL69-8H_xcUZBSZLiWthgLQPZvqPi"},
+                    User = user1
+                },
+                movieToDelete,
+            };
+
+            mock.Setup(m => m.GetAll()).Returns(() => movies);
+
+            mock.Setup(m => m.MovieExists("The Equalizer 3")).Returns(() => true);
+            mock.Setup(m => m.MovieExists("John Wick: Chapter 4")).Returns(() => true);
+            mock.Setup(m => m.MovieExists("Oppenheimer")).Returns(() => true);
+
+            mock.Setup(m => m.MovieExists("Fast X")).Returns(() => false);
+
+            mock.Setup(m => m.AddMovie(movieToAdd)).Callback<Movie>((_movie) => movies.Add(_movie));
+
+            mock.Setup(m => m.Delete(username, movieToDelete.name)).Callback(() => movies.RemoveAt(2));
+            mock.Setup(m => m.Delete("User4", "John Wick: Chapter 3"));
+
+
+            mock.Setup(m => m.GetWatchListByUserName(username)).Returns(() => movies.GetRange(0, 2));
+
+
+            return mock;
+        }
+    }
+
+    
+    [TestMethod]
+    public void TestAdd()
+    {
+        var storeWrapper = MockStoreWrapper.GetMock();
+        var store = storeWrapper.Object;
+        store.AddMovie(MockStoreWrapper.movieToAdd);
+        Assert.IsTrue(store.GetAll().Contains(MockStoreWrapper.movieToAdd));
+        Assert.AreEqual(4, store.GetAll().Count());
+    }
+
+
+
+
 }
+
+}
+
 

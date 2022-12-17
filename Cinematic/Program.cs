ee6380b757f3b38f9519869443eeba5e27420b69
Shubham Dhamane <105388746+dhamanes7@users.noreply.github.com>
2022-12-16 23:08:43 -0500

    login has been added

--- Cinematic/Program.cs
+++ Cinematic/Program.cs
@@ -3,10 +3,15 @@ using Microsoft.Extensions.DependencyInjection;
 using Cinematic;
 using Cinematic.Models;
 using CinematicLibrary.ApiClient;
+using Microsoft.AspNetCore.Identity;
+using Cinematic.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<Data>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Data") ?? throw new InvalidOperationException("Connection string 'Data' not found.")));
+
+builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<MovieDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -27,6 +32,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
@@ -35,5 +41,6 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
+app.MapRazorPages();
 
 app.Run();
